e17df0b10b94dd35d340ff7c3f47be220ad51534
Timcu Alexei <a.timcu@studii.md>
2021-05-31 12:02:42 +0300

    rendomize classname production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-const u = '123';
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there!!!!!</h1>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
