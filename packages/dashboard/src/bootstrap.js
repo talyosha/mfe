5f14fa0a5921f78c3d04fc2c090255974c736a44
Timcu Alexei <a.timcu@studii.md>
2021-06-01 10:43:36 +0300

    deploy

--- packages/dashboard/src/bootstrap.js
+++ packages/dashboard/src/bootstrap.js
@@ -9,7 +9,6 @@ const mount = (el) => {
 
 // If we are in development and in isolation,
 // call mount immediately
-// jj
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_dashboard-dev-root');
 
