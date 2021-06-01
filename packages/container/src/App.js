63045143aee677a99668b1105e84c6a18c43f56e
Timcu Alexei <a.timcu@studii.md>
2021-06-01 10:03:01 +0300

    add dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -9,6 +9,7 @@ import Header from './components/Header';
 
 const MarketingLazy = lazy(() => import('./components/MarketingApp'))
 const AuthLazy = lazy(() => import('./components/AuthApp'))
+const DashboardLazy = lazy(() => import('./components/DashboardApp'))
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
